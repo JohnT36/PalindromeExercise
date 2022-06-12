df10f3b7d209c56110ad9cd107a23e5369f9ac6a
John Tuck <j.tuck1336@gmail.com>
2022-06-12 09:47:09 -0400

    Finished Exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,9 +5,16 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("starfire", false)]
+        public void IsPalindromeTest(string word, bool expected)
         {
+            var WordSmith = new WordSmith();
+
+            var actual = WordSmith.IsPalindrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExerciseTests
+{
+    internal class WordSmith
+    {
+
+        public bool IsPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            { reversed += word[i]; }
+            if (reversed == word)
+            { return true; }
+            return false;
+        }
+    }
+}
